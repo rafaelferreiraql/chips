5881ff407a6a977593c9daec2707f78cf0403102
rafaelferreiraql <raf4elferreira@gmail.com>
2017-02-14 10:32:36 -0200

    Remove unused files

--- assets/playeralt.js
+++
@@ -1,8 +0,0 @@
-// Made just in case the first route (ES6 classes) doesn't work out
-
-let player = {
-    test: function() {console.log(Graphics);}
-}
-
-let player1 = Object.create(player);
-let player2 = Object.create(player);
